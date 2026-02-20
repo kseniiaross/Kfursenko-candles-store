6e391b296a9e329451a5b93aeed814b5810dad7a
rostovks94 <rostovsskaya@gmail.com>
2026-02-20 18:18:22 -0500

    update

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,10 +3,9 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import RegisterAPIView, CustomTokenObtainPairView, ProfileAPIView
 
-
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
+    path("register/", RegisterAPIView.as_view(), name="register"),
+    path("login/", CustomTokenObtainPairView.as_view(), name="login"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/", ProfileAPIView.as_view(), name="profile"),
